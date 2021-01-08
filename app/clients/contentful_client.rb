1131e9600b24a51d4de4620023ba95d9f3342b32
Alex <alexalexalexalexalexalexalexalexalex@example.com>
2021-01-08 12:24:57 +0300

    Contentful client added

--- app/clients/contentful_client.rb
+++ app/clients/contentful_client.rb
@@ -1,21 +1,23 @@
 require 'contentful_client'
 
-class ContentfulClient < SimpleDelegator
-  include Singleton
+module Clients
+  class ContentfulClient < SimpleDelegator
+    include Singleton
 
-  def initialize
-    client = set_client
-    super(client)
-  end
-
-  private
-
-    def set_client
-      Contentful::Client.new(
-        access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
-        space: ENV['CONTENTFUL_SPACE_ID'],
-        dynamic_entries: :auto,
-        raise_errors: true
-      )
+    def initialize
+      client = set_client
+      super(client)
     end
+
+    private
+
+      def set_client
+        Contentful::Client.new(
+          access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
+          space: ENV['CONTENTFUL_SPACE_ID'],
+          dynamic_entries: :auto,
+          raise_errors: true
+        )
+      end
+  end
 end
