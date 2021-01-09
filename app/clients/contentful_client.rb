ee90d63d2fa4099a587388b7a3230414c90e8d37
Alex <alexalexalexalexalexalexalexalexalex@example.com>
2021-01-09 19:01:06 +0300

    logger specification added

--- app/clients/contentful_client.rb
+++ app/clients/contentful_client.rb
@@ -16,6 +16,7 @@ module Clients
           access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
           space: ENV['CONTENTFUL_SPACE_ID'],
           dynamic_entries: :auto,
+          logger: Rails.logger,
           raise_errors: true
         )
       end
