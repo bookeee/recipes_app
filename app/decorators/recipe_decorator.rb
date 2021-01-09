6c5d250ad8ce4a5f43c0b414ebbfcfb96e6e3bb1
Alex <alexalexalexalexalexalexalexalexalex@example.com>
2021-01-09 14:58:23 +0300

    decorator for recipes added

---
+++ app/decorators/recipe_decorator.rb
@@ -0,0 +1,29 @@
+class RecipeDecorator < Draper::Decorator
+  include MarkdownHelper
+
+  delegate_all
+
+  def image_url
+    object.photo.url
+  end
+
+  def chef_name
+    if object.fields[:chef]
+      object.chef&.name
+    else
+      'Unknown Chef'
+    end
+  end
+
+  def tags
+    if object.fields[:tags]
+      object.tags.map(&:name).join(', ')
+    else
+      'No tags'
+    end
+  end
+
+  def decription_html
+    parse_markdown(object.description)
+  end
+end
