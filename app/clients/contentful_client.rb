6dddc6eccda1982f349799f993be273e3e6c7054
Alex <alexalexalexalexalexalexalexalexalex@example.com>
2021-01-12 08:51:56 +0300

    ContentfulClient logic changed, 2 ways on init for different environments

--- app/clients/contentful_client.rb
+++ app/clients/contentful_client.rb
@@ -4,21 +4,26 @@ module Clients
   class ContentfulClient < SimpleDelegator
     include Singleton
 
+    TEST_ENV_CONFIG = { access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
+                        space: ENV['CONTENTFUL_SPACE_ID'] }.freeze
+    DEV_ENV_CONFIG = { access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
+                       space: ENV['CONTENTFUL_SPACE_ID'],
+                       dynamic_entries: :auto,
+                       logger: Rails.logger, raise_errors: true }.freeze
+
     def initialize
-      client = set_client
-      super(client)
+      client
+      super(@client)
     end
 
     private
 
-      def set_client
-        Contentful::Client.new(
-          access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
-          space: ENV['CONTENTFUL_SPACE_ID'],
-          dynamic_entries: :auto,
-          logger: Rails.logger,
-          raise_errors: true
-        )
+      def client
+        @client ||= if Rails.env.test?
+                      Contentful::Client.new(TEST_ENV_CONFIG)
+                    else
+                      Contentful::Client.new(DEV_ENV_CONFIG)
+                    end
       end
   end
 end
